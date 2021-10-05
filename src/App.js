bdd63c8932a68ccca0553ab207e5f5a8431b4b6b
yoshiodei <yoshiodei@yahoo.com>
2021-10-05 14:28:29 +0000

    modify app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import Nav from "./component/Nav";
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+import Blog from "./component/Blog";
+import BlogList from "./component/BlogList";
+import NewBlogForm from "./component/NewBlogForm";
+import EditBlogForm from "./component/EditBlogForm";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+       <Nav />
+       <Switch>
+       <Route path="/" exact>
+            <BlogList />
+         </Route>
+         <Route path="/blog/:id" exact>
+            <Blog />
+         </Route>
+         <Route path="/create" exact>
+            <NewBlogForm />
+         </Route>
+         <Route path="/edit/:id" exact>
+            <EditBlogForm />
+         </Route>
+         <Route path="*" exact>
+            <EditBlogForm />
+         </Route>
+       </Switch>
+    </Router>
+    
   );
 }
 
