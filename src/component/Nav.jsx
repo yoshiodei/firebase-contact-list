4e3edc33a65816d3080bad25d380e94f3e09eafb
yoshiodei <yoshiodei@yahoo.com>
2021-10-05 14:44:03 +0000

    modify Nav component

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 const Nav = () => {
     return (
         <nav className="nav" >
-           <Link to='/'><h1>My Blog Page</h1></Link> 
+           <Link to='/'><h1>Yoshi's Blog Page</h1></Link> 
            <Link to='/create'><h1>New Post</h1></Link> 
         </nav>
     );
