84367581407219acc28a31c29f46bdf0db7fd567
yoshiodei <yoshiodei@yahoo.com>
2021-10-10 15:27:51 +0000

    modify Nav component

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -1,10 +1,24 @@
 import { Link } from 'react-router-dom';
+import { logout } from '../action/action';
+import { useHistory } from 'react-router';
+import { useDispatch } from 'react-redux';
 
 const Nav = () => {
+
+   const dispatch = useDispatch();
+   const { replace } = useHistory();
+
+   const handleClick = () => {
+       dispatch(logout(replace));
+   }
+
     return (
         <nav className="nav" >
-           <Link to='/'><h1>Yoshi's Blog Page</h1></Link> 
-           <Link to='/create'><h1>New Post</h1></Link> 
+           <Link to='/home'><h1>Yoshi's Blog Page</h1></Link> 
+           <div className="nav-div">
+                <Link to='/create'><h1>New Post</h1></Link>
+                <button onClick={handleClick}>Log Out</button> 
+           </div>
         </nav>
     );
 }
---
+++ src/component/Nav2.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+
+const Nav2 = () => {
+    return (
+        <nav className="nav nav2" >
+           <Link to='/'><h1>Yoshi's Blog Page</h1></Link> 
+        </nav>
+    );
+}
+
+export default Nav2;
