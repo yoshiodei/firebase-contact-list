18f1dcd2a484dd222e4e4a98aa42a7cc37316457
yoshiodei <yoshiodei@yahoo.com>
2021-10-10 15:08:52 +0000

    add Nav2 component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,44 @@
 import Nav from "./component/Nav";
+import Nav2 from "./component/Nav2";
 import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
 import Blog from "./component/Blog";
 import BlogList from "./component/BlogList";
 import NewBlogForm from "./component/NewBlogForm";
 import EditBlogForm from "./component/EditBlogForm";
+import PageNotFound from "./component/PageNotFound";
 import RegisterUser from "./component/RegisterUser";
 import Login from "./component/Login";
 
 function App() {
   return (
     <Router>
-       <Nav />
        <Switch>
          <Route path="/" exact>
+            <Nav2 />
             <RegisterUser />
          </Route> 
          <Route path="/login" exact>
+            <Nav2 />
             <Login />
          </Route>   
          <Route path="/home" exact>
+            <Nav />
             <BlogList />
          </Route>
          <Route path="/blog/:id" exact>
+            <Nav />
             <Blog />
          </Route>
          <Route path="/create" exact>
+            <Nav />
             <NewBlogForm />
          </Route>
          <Route path="/edit/:id" exact>
+            <Nav />
             <EditBlogForm />
          </Route>
          <Route path="*" exact>
-            <EditBlogForm />
+            <PageNotFound />
          </Route>
        </Switch>
     </Router>
